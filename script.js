a1bae5b0b2f071d550a11705b389f31155dc6710
Kbuczynski <44814273+Kbuczynski@users.noreply.github.com>
2019-06-04 15:46:37 +0200

    change concept

--- script.js
+++ script.js
@@ -1,151 +1,188 @@
-var canvas = document.getElementById('canvas');
-var c = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const c = canvas.getContext("2d");
 
-var nick = '';
-
-var pageX = window.innerWidth;
-var pageY = window.innerHeight;
-
-var playerSize = 10;
-var rectSize = 5;
+let pageX = window.innerWidth;
+let pageY = window.innerHeight;
 
 //mouse position
-var x = pageX/2;
-var y = pageY/2;
+let x = pageX / 2;
+let y = pageY / 2;
 
-var moveX = 1;
-var moveY = 1;
+let moveX = 1;
+let moveY = 1;
 
-//ball position
-var ballX = 800; 
-var ballY = 200;
-
-var secunds = 0;
-
+//bonus balls
 var bonusBallSize = 5;
-var numberOfBalls = Math.floor((Math.random() * 5) + 2);
+var numberOfBalls = Math.floor(Math.random() * 5 + 2);
 
 const positionBallsX = [];
 const positionBallsY = [];
 
-var ballsObject = bonusBalls();
+var ballsObject = generateBonusBalls();
 
-var timerObject = score();
+class Player {
+  constructor(nick, color) {
+    this.nick = nick;
+    this.color = color;
+    this.score = 0;
+    this.isLives = true;
+    this.sizeBall = 10;
+    this.sizeRect = 5;
+    this.ballX = Math.floor(Math.random() * pageX);
+    this.ballY = Math.floor(Math.random() * pageY);
+  }
 
-function draw() {
-    //responsive canvas
-    canvas.setAttribute("width", pageX - 20);
-    canvas.setAttribute("height", pageY - 20);
+  draw() {
+    //ball
+    c.arc(this.ballX, this.ballY, this.sizeBall, 0, 10 * Math.PI);
+    c.fillStyle = this.color;
+    c.fill();
 
-    //background
-    c.fillStyle = "#fff";
-    c.fillRect(0, 0, 1000, 600);
-    c.strokeRect(0, 0, pageX - 20, pageY - 20);
+    //rect
+    c.fillRect(
+      this.ballX - this.sizeBall / 2,
+      this.ballY + this.sizeBall / 2,
+      this.sizeRect,
+      this.sizeRect
+    );
+    c.fill();
     c.beginPath();
 
-    //bonus balls
-    for (var i = 0; i < numberOfBalls; i++) {
-        c.arc(ballsObject.x[i], ballsObject.y[i], bonusBallSize, 0, 10 * Math.PI);
-        c.fillStyle = "#" + Math.floor((Math.random() * 9) + 1) + Math.floor((Math.random() * 9) + 1) + Math.floor((Math.random() * 9) + 1);
-        c.fill();
-        c.beginPath();
-    }
-
-    //player
-    c.textAlign = "center";
-    // c.drawImage(document.querySelector("#avatar1"), ballX - 10, ballY, playerSize, playerSize);
-    c.arc(ballX, ballY, playerSize, 0, 10 * Math.PI);
-    c.fillStyle = "#f00";
-    c.fill();
-
-    c.fillRect(ballX - playerSize/2, ballY + playerSize/2, rectSize, rectSize);
-    c.fill();
-
+    //nick name
     c.font = "20px sans-serif";
     c.textAlign = "center";
-    c.strokeText(nick, ballX, ballY);
+    c.strokeStyle = "#000";
+    c.strokeText(this.nick, this.ballX, this.ballY);
     c.beginPath();
+  }
 
-    //move speed
-    if (i != x) {
-        moveX=(ballX-x)/playerSize;
-        moveY=(ballY-y)/playerSize;
-        ballX=ballX-moveX;
-        ballY=ballY-moveY;
+  move() {
+    moveX = (this.ballX - x) / this.sizeBall;
+    moveY = (this.ballY - y) / this.sizeBall;
+    this.ballX = this.ballX - moveX;
+    this.ballY = this.ballY - moveY;
+  }
+
+  growing() {
+    if (this.sizeBall <= 50) {
+      this.sizeBall++;
+      this.sizeRect++;
+    }
+  }
+
+  collision(enemy) {
+    //collision with balls
+    for (let i = 0; i < numberOfBalls; i++) {
+      if (
+        ballsObject.x[i] <= this.ballX + this.sizeBall / 2 &&
+        ballsObject.x[i] >= this.ballX - this.sizeBall / 2 &&
+        ballsObject.y[i] <= this.ballY + this.sizeBall / 2 &&
+        ballsObject.y[i] >= this.ballY - this.sizeBall / 2
+      ) {
+        this.sizeBall = 10;
+        this.sizeRect = 5;
+        positionBallsX.splice(i, 1);
+        positionBallsY.splice(i, 1);
+      }
     }
 
-    //score
-    c.font = "60px sans-serif";
-    c.strokeText("Score: " + secunds, pageX/2 - pageX*0.07, 60);
-    c.beginPath();
-
-    c.closePath();
-
-    detectCollision();
+    //collision with enemy
+    if (
+      enemy.ballX - enemy.sizeBall / 2 + enemy.sizeRect <=
+      this.ballX + this.sizeBall / 2
+    ) {
+      console.log("test");
+    }
+  }
 }
+
+//main player object
+let MainPlayer = new Player("", "");
+
+//bots objects
+const Bot1 = new Player("Bot", "red");
 
 function play() {
-    nick = document.querySelector("input").value;
-    document.querySelector("#canvas").style.display = "inline";
-    document.querySelector("section").style.display = "none";
-    main();
+  MainPlayer = new Player(
+    document.getElementById("nick").value,
+    document.getElementById("color").value
+  );
+
+  document.querySelector("section").style.display = "none";
+
+  main();
 }
 
-function score() {
-    return secunds++;
+function draw() {
+  //responsive canvas
+  canvas.setAttribute("width", pageX - 20);
+  canvas.setAttribute("height", pageY - 20);
+
+  //bonus balls
+  drawBonusBalls();
+
+  //player
+  MainPlayer.draw();
+  MainPlayer.move();
+
+  Bot1.draw();
+
+  //detect collision
+  MainPlayer.collision(Bot1);
+
+  c.closePath();
 }
 
-function growingPlayer() {
-    if (playerSize <= 300) {
-        playerSize++
-        rectSize++;
-    }
+function generateBonusBalls() {
+  positionBallsX.splice(0, numberOfBalls);
+  positionBallsY.splice(0, numberOfBalls);
+
+  for (var i = 0; i < numberOfBalls; i++)
+    positionBallsX.push(Math.floor(Math.random() * pageX + 1));
+  for (var i = 0; i < numberOfBalls; i++)
+    positionBallsY.push(Math.floor(Math.random() * pageY + 1));
+
+  return {
+    x: positionBallsX,
+    y: positionBallsY
+  };
 }
 
-function detectCollision() {
-    for (var i = 0; i < numberOfBalls; i++) {
-        if (ballsObject.x[i] < ballX + playerSize/2 && ballsObject.x[i] > ballX - playerSize/2
-            && ballsObject.y[i] < ballY + playerSize/2 && ballsObject.y[i] > ballY - playerSize/2) {
-                playerSize = 10;
-                rectSize = 5;
-                positionBallsX.splice(i, 1);
-                positionBallsY.splice(i, 1);
-            }
-    }
-}
-
-function bonusBalls() {
-    positionBallsX.splice(0, numberOfBalls);
-    positionBallsY.splice(0, numberOfBalls);
-
-    for (var i = 0; i < numberOfBalls; i++) positionBallsX.push(Math.floor(Math.random() * pageX + 1));
-    for (var i = 0; i < numberOfBalls; i++) positionBallsY.push(Math.floor(Math.random() * pageY + 1));
-
-    return {
-        x: positionBallsX,
-        y: positionBallsY
-    }; 
+function drawBonusBalls() {
+  for (var i = 0; i < numberOfBalls; i++) {
+    c.arc(ballsObject.x[i], ballsObject.y[i], bonusBallSize, 0, 10 * Math.PI);
+    c.fillStyle =
+      "#" +
+      Math.floor(Math.random() * 9 + 1) +
+      Math.floor(Math.random() * 9 + 1) +
+      Math.floor(Math.random() * 9 + 1);
+    c.fill();
+    c.beginPath();
+  }
 }
 
 //report the mouse position on move
-canvas.addEventListener("mousemove", function (evt) {
+canvas.addEventListener(
+  "mousemove",
+  function(evt) {
     var mousePos = getMousePos(canvas, evt);
     x = mousePos.x;
     y = mousePos.y;
-}, false);
-
+  },
+  false
+);
 
 //get mouse position
 function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-        x: evt.clientX - rect.left,
-        y: evt.clientY - rect.top
-    };
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
 }
 
 function main() {
-    setInterval('draw()', 10);
-    setInterval('growingPlayer(), score()', 1000);
-    setInterval('bonusBalls()', 30000);
+  setInterval("draw()", 10);
+  setInterval("MainPlayer.growing(), Bot1.growing()", 1000);
+  setInterval("generateBonusBalls()", 30000);
 }
